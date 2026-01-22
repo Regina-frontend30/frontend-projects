30b11cb539867773e57e4219b120f9dba79d5c46
Regina Frontend <reginakarimovna1330@gmail.com>
2026-01-22 14:49:05 +0600

    add task 2: username validation with try-catch

---
+++ basics/error-handling-and-network/task-2-username-validation/index.js
@@ -0,0 +1,15 @@
+function greeting() {
+  const username = prompt("Введите имя пользователя");
+
+  if (!username) {
+    throw new Error("Имя обязательно для заполнения");
+  }
+
+  console.log(`Привет, ${username}!`);
+}
+
+try { 
+  greeting();
+} catch (error) { 
+  alert(error.message);
+}
