ae938fe94d3c02f5ad70d0fe43645e69b391de3d
Regina Frontend <reginakarimovna1330@gmail.com>
2026-01-27 14:07:06 +0600

    add delivery list with classes module

---
+++ basics/delivery-list-with-classes/task-1-delivery-list-with-classes/index.js
@@ -0,0 +1,14 @@
+import Delivery from "./Delivery.js";
+
+const deliveryArr = [
+  new Delivery("Ольга", "ул. Вымыслов, д. 12", 8),
+  new Delivery("Дмитрий", "ул. Задачная, д. 7", 3),
+  new Delivery("Оля", "ул. Ткачей, д. 43", 11),
+];
+
+const listEl = document.querySelector("#delivery-list");
+
+deliveryArr.forEach((delivery) => {
+  const cardEl = delivery.createCardElement();
+  listEl.append(cardEl);
+});
